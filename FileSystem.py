8cbc5000e325884e570a4790b855c5377d93c8ef
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-04-15 11:49:04 -0600

    created download function & renamed GUI.py

--- FileSystem.py
+++ FileSystem.py
@@ -1,5 +1,39 @@
-from customtkinter import filedialog;
-# Move Filedialog code from GUI to this file
+import customtkinter as ctk;
+from customtkinter import filedialog as fd;
+import os;
 
-def downloadFile():
-    print("File downloaded")
+def DownloadFileGUI(filename, contents):
+    dir = fd.askdirectory(title="Select File download location")
+    saveFile = dir + "/" + os.path.basename(filename)
+    
+    if(filename and contents):
+        try:
+            CreateFile(saveFile, contents.get(0.0, ctk.END))
+        except FileExistsError as e:
+            print(f"An error occured while creating a file {e}")
+    else:
+        print("No file is selected or there are no contents displayed")
+
+def SelectFileGUI(pathTxt):
+    path = fd.askopenfilename(filetypes=[("Python Files", "*.py")])
+    pathTxt.delete(0, ctk.END)
+    pathTxt.insert(0, path)
+
+def DisplayFileGUI(filepath, display):
+    if(filepath and display):
+        display.delete(1.0, ctk.END)
+        
+        try:
+            with open(filepath, 'r') as file:
+                display.insert(1.0, file.read())
+                
+        except Exception as e:
+            display.insert(1.0, f"Error opening file: {e}")
+
+def CreateFile(filepath, content):
+    if filepath:
+        if os.path.exists(filepath):
+            print("This file already exsists")
+        else:
+            with open(filepath, 'w') as f:
+                f.write(content)
