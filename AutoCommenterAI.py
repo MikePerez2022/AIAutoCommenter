6e61a1983bb4a39a28b3497834953b67d59bc767
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-05-13 19:53:56 -0600

    switching to a new bigger model

--- AutoCommenterAI.py
+++ AutoCommenterAI.py
@@ -1,25 +1,25 @@
-from llama_cpp import Llama
+#from llama_cpp import Llama
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
 
 # Path to your downloaded GGUF model
-model_path = r"C:\Users\Mike\Desktop\Projx\AIAutoCommenter\Models\llama-3.2-1b-instruct-q4_k_m.gguf"
-
-llm = Llama(model_path=model_path, n_ctx=2048, n_threads=6)
+#model_path = r"C:\Users\Mike\Desktop\Projx\AIAutoCommenter\Models\llama-3.2-1b-instruct-q4_k_m.gguf"
+#llm = Llama(model_path=model_path, n_ctx=2048, n_threads=6)
 
 def comment_code(code: str) -> str:
-    prompt = f"""You will add descriptive pthon comments to this Python code.
+    prompt = f"""You will add descriptive python comments to this Python code.
     ### Original Code:
     {code}
     ### Commented Code:
     """
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(**inputs, max_new_tokens=100)
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_new_tokens=300, do_sample=True, temperature=0.7)
     #output = llm(prompt, echo=False, max_tokens=800)
     #return output["choices"][0]["text"]
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return output_text[len(prompt):].strip()
 
 def AIComment(code) -> str:
     output = comment_code(code)
